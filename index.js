8acca23cbef2a5c3cafa3c77a03569c9c3e6cecf
Okwor Chukwuemeka Nnajiofor <88171212+thenaijadev@users.noreply.github.com>
2022-06-24 12:31:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-function add(a, b) {
-  return a + b;
-}
-
-function substract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-function calculate(a, b, operation) {
-  return operation(a, b);
-}
-
-let ans = calculate(2, 3, multiply);
-console.log(ans);
+const operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');  
+  
+// accept the number from the user through prompt box  
+const number1 = parseFloat(prompt ('Enter the first number: '));  
+const number2 = parseFloat(prompt ('Enter the second number: '));  
+  
+let result; // declaration of the variable.  
+  
+// use if, elseif and else keyword to define the calculator condition in JavaScript.  
+if (operator == '+') { // use + (addition) operator to add two numbers  
+    result = number1 + number2;  
+}  
+else if (operator == '-') { // use -  (subtraction) operator to subtract two numbers  
+    result = number1 - number2;  
+}  
+else if (operator == '*') { // use * (multiplication) operator to multiply two numbers  
+    result = number1 * number2;  
+}  
+else {  
+    result = number1 / number2; // use / (division) operator to divide two numbers  
+}  
+  
+// display the result of the Calculator  
+window.alert(" Result is " + result);  
